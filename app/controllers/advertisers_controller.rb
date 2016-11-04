7baa817d8884ea344de9c269c08a4c7658f86daf
apoorv <apoorv.coder@live.com>
2016-11-04 11:56:49 +0530

    advertiser sign up process done

--- app/controllers/advertisers_controller.rb
+++ app/controllers/advertisers_controller.rb
@@ -1,7 +1,24 @@
 class AdvertisersController < ApplicationController
 
-  before_filter :authenticate_advertiser!
+  before_filter :authenticate_advertiser!, :except =>[:new, :create]
+
   def index
     @current_advertisers_data = current_advertiser
   end
+
+  def new
+    @new_advertiser = Advertiser.new
+  end
+
+  def create
+    Advertiser.create!(advertiser_params)
+    redirect_to "/advertisers"
+  end
+
+  private
+
+  def advertiser_params
+    params.require(:advertiser).permit!
+  end
+
 end
