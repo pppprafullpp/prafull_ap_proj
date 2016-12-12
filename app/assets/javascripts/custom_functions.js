daba53da8586a7b448ec05040db9a90536ebf8b8
apoorv <apoorv.coder@live.com>
2016-12-12 12:41:04 +0530

    ad decline flow

--- app/assets/javascripts/custom_functions.js
+++ app/assets/javascripts/custom_functions.js
@@ -51,3 +51,38 @@ FB.login(function(response) {
   console.log(response);
 }, {scope: 'publish_actions',auth_type: 'reauthenticate',});
 }
+function decline_ad_by_influencer(){
+  event.preventDefault();
+  ad_id = $("#declined_ad_id").val();
+  ad_decline_reason = $("#reason_for_ad_decline").val();
+  console.log(ad_id+""+ad_decline_reason);
+  $.ajax({
+    url:"/advertisments/ad_declined_by_influencer/",
+    method:"post",
+    data: {
+      ad_id:ad_id,
+      reason_for_decline:ad_decline_reason
+    },
+    success:function(response){
+      if (response["success"]) {
+        toastr.success("updated");
+        $("#declinePopup").modal("hide");
+      }
+      else {
+        toastr.info("error");
+        $("#declinePopup").modal("hide");
+      }
+    },
+    error:function(response){
+      console.log(response);
+      if (response["success"]) {
+        toastr.success("updated");
+      }
+      else {
+        toastr.info("error");
+        $("#declinePopup").modal("hide");
+      }
+    }
+  });
+  console.log(ad_decline_reason);
+}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :declined_by_influencer, :approved_by_admin
 
   def sign_up_params
     devise_parameter_sanitizer.sanitize(:sign_up)
@@ -16,5 +17,15 @@ class ApplicationController < ActionController::Base
     b = ('0'..'9').to_a.shuffle.first(10).join
     token = a + b
     token
+  end
+
+  def declined_by_influencer
+    value = Advertisement::STATUS["Declined by influencer"]
+    value
+  end
+
+  def approved_by_admin
+    value = Advertisement::STATUS["Approved by Admin"]
+    value
   end
 end
