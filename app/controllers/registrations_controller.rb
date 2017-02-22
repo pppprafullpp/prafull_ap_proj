8b0bf74484b3edb1937246df7aa67f3c2cf9c5f3
apoorv <apoorv.coder@live.com>
2017-02-22 17:44:22 +0530

    mailing issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,13 +10,13 @@ class RegistrationsController < ApplicationController
       new_advertiser = Advertiser.create!(advertiser_params)
       activation_token = ApplicationController.new.generate_activation_token
       new_advertiser.update_attributes(:token=> activation_token)
-      # NotificationMailer.welcome_mail("advertiser",new_advertiser.id).deliver!
+      NotificationMailer.welcome_mail("advertiser",new_advertiser.id).deliver!
     else
       new_influencer = Influencer.new(influencer_params)
       activation_token = ApplicationController.new.generate_activation_token
       new_influencer.token = activation_token
       new_influencer.save!
-      # NotificationMailer.welcome_mail("influencer",new_influencer.id).deliver!
+      NotificationMailer.welcome_mail("influencer",new_influencer.id).deliver!
     end
     flash[:success] = "Successfully signed up, please login"
     forward_to = params[:advertiser].present? ? "/advertisers/sign_in" :  "/influencers/wizard"
